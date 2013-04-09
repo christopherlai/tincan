0286dc399a32aece69aacf64e2884485734c5d7d
Aaron Gotwalt <gotwalt@seesaw.co>
2013-04-09 14:15:56 -0700

    Unborked verify test

--- spec/integration/verify_spec.rb
+++ spec/integration/verify_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe 'Verify integration' do
   it 'should require a code' do
-    post 'v1/verify'
+    post 'v1/phone_numbers/verify'
     last_status.must_equal 400
   end
 
   it 'should require a valid code' do
-    post 'v1/verify', code: 'asdf'
+    post 'v1/phone_numbers/verify', code: 'asdf'
     last_status.must_equal 400
 
     phone = Tincan::PhoneNumber.create!('+14152751660')
-    post 'v1/verify', code: phone.code
+    post 'v1/phone_numbers/verify', code: phone.code
     last_status.must_equal 200
 
     get "v1/phone_numbers/#{phone.id}"
