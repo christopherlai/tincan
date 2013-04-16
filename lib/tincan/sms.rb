a835f31d0896282f283f2d48ffa6223f109052a1
Sam Soffes <sam@soff.es>
2013-04-16 22:08:31 +0300

    No need to globally replace here

--- lib/tincan/sms.rb
+++ lib/tincan/sms.rb
@@ -5,7 +5,7 @@ module Tincan
 
       def send(number, message)
         raise NoSmsSenderError.new unless @sender
-        @sender.call number.e164, message.gsub('CODE', number.code)
+        @sender.call number.e164, message.sub('CODE', number.code)
       end
     end
 
